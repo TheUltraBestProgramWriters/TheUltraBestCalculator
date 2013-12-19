cc13a4111acbb6d2576f3c23f0af7f07b60d2174
mailmanbam <liaml2@hotmail.com>
2013-12-19 15:49:21 +1100

    Create BootCalculator.py

---
+++ BootCalculator.py
@@ -0,0 +1,9 @@
+#start
+def all_systems_go()
+  print "Welcome to the TheUltraBestCalculator calculator. Please insert your first number"
+  number1 = raw_input()
+  print "Thanks for inserting %s, now insert your second number" %number1
+  number2 = raw_input()
+  print "Crunching numbers, CPU usage now 100%, beware of spontanious CPU combustion"
+  answer == number1 + number2
+  print "The answer is %s, thanks you for using the TheUltraBestCalculator calculator. Goodbye" %answer
